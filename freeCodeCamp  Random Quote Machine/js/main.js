dcf988c9f99859f64a738098ee93fe5c67633b1c
sahil <sahillamba05@gmail.com>
2017-04-24 01:52:40 +0530

    quote.html updated

--- freeCodeCamp  Random Quote Machine/js/main.js
+++ freeCodeCamp  Random Quote Machine/js/main.js
@@ -1,16 +1,18 @@
 
-setTimeOut(function(){
-  $(document).ready(function(){
-    $(".buttons").click(function(e){
 
-      $.ajax({
-              url: "https://quotesondesign.com/wp-json/posts?",
-            data: "filter[orderby]=rand&filter[posts_per_page]=1&callback=",
-            success: function(data){
-        var c=data.shift();
-        console.log(c.content);
-        $(".card-block").html("<p"+c.content+"</p>");
-      }});
-    });
-  });
-},8000);
+$(document).ready(function(){
+
+  setTimeOut(Randomquote(),8000);
+  
+  function Randomquote(){
+
+    $.ajax({
+            url: "https://quotesondesign.com/wp-json/posts?",
+          data: "filter[orderby]=rand&filter[posts_per_page]=1&callback=",
+          success: function(data){
+      var c=data.shift();
+      console.log(c.content);
+      $(".card-block").html("<p"+c.content+"</p>");
+    }});
+  };
+});
